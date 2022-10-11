6fd606c4c6079033bedb31ced7087a2f8b9cbfb2
Loghic <michalek.matej.loghin@gmail.com>
2022-10-11 23:50:38 +0200

    Finished comparison.cpp

--- ZPRO/cvika/cv4/comparison.cpp
+++ ZPRO/cvika/cv4/comparison.cpp
@@ -3,58 +3,93 @@
 
 using namespace std;
 
-int compare(const char*, const char*, const int, const int);
+struct Strings
+{
+    string str1;
+    string str2;
+    int lenghtStr1;
+    int lenghtStr2;
+    int longerStr;
+    int shorterStr;
+    void print(int compareResult)
+    {
+        if (compareResult == 1){
+            cout << "The first string :'" << str1 << "' is bigger!" << endl;
+        }else if (compareResult == -1){
+            cout << "The second string: '" << str2 << "' is bigger!" << endl;
+        }else{
+            cout << "'" << str1 << "' and '" << str2 << "' are equal"<< endl;
+        }
+    }
+    void input()
+    {
+        cout << "Please enter 1st string: ";
+        cin >> str1;
+        cout << "Please enter 2nd string: ";
+        cin >> str2;
+        lenghtStr1 = str1.size();
+        lenghtStr2 = str2.size();
+        bigger();
+    }
+
+    void bigger()
+    {
+        if (lenghtStr1 > lenghtStr2){
+            longerStr = lenghtStr1;
+            shorterStr = lenghtStr2;
+        }else{
+            longerStr = lenghtStr2;
+            shorterStr = lenghtStr1;
+        }
+    }
+};
+
+// compare result '1' 1. is bigger, '0' they are equal, '-1' the second is bigger
+int compare(Strings);
 
 int main()
 {
-    const char* x = "122.22";
-    const char* y = "130";
-    int lenghtX = strlen(x);
-    int lenghtY = strlen(y);
-    int cmpRes = compare(x,y,lenghtX,lenghtY); // compare result '1' 1. is bigger, '0' they are equal, '-1' the second is bigger
-    if (cmpRes == 1){
-        cout << "The first string is bigger!" << endl;
-    }else if (cmpRes == -1){
-        cout << "Second string is bigger!" << endl;
-    }else{
-        cout << "They are equal!" << endl;
-    }
+    Strings str;
+    str.input();
+    str.print(compare(str));
+
     return 0;
 }
 
-int compare(const char* x, const char* y, const int lenghtX, const int lenghtY)
+int compare(Strings str)
 {
     int ans = 0;
-    int posPointX = 0;
-    int posPointY = 0;
+    int decimalPointPosStr1 = str.lenghtStr1;
+    int decimalPointPosStr2 = str.lenghtStr2;
     bool isBigger = false;
-    for (size_t i{0}; i < lenghtX && i < lenghtY; i++){
-        if (!isBigger){
-            if (x[i] > y[i]){
+    for (size_t i{0}; i < str.longerStr; i++){
+        if (i < str.shorterStr && !isBigger){
+            if (str.str1[i] > str.str2[i]){
                 ans = 1;
                 isBigger = true;
-            }else if (y[i] > x[i]){
+            }else if (str.str2[i] > str.str1[i]){
                 ans = -1;
                 isBigger = true;
             }
         }
-        if (x[i] == '.'){
-            posPointX = i;
+        if (i < str.lenghtStr1 && str.str1[i] == '.'){
+            decimalPointPosStr1 = i;
         }
-        if (y[i] == '.'){
-            posPointY = i;
+        if (i < str.lenghtStr2 && str.str2[i] == '.'){
+            decimalPointPosStr2 = i;
         }
     }
-    if (posPointX > posPointY){
-        ans = -1;
-    }else if (posPointY > posPointX){
-        ans = 1; 
+    if (decimalPointPosStr1 > decimalPointPosStr2){
+        ans = 1;
+    }else if (decimalPointPosStr2 > decimalPointPosStr1){
+        ans = -1; 
     }else{
-        if (lenghtX > lenghtY){
+        if (str.lenghtStr1 > str.lenghtStr2){
             ans = 1;
-        }else if (lenghtY > lenghtX){
+        }else if (str.lenghtStr2 > str.lenghtStr1){
             ans = -1;
         }
     }
     return ans;
 }
+

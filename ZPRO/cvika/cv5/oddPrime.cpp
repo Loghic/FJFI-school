06065b3756a50f7f92dbbee038bbfad3d10462ef
Loghic <michalek.matej.loghin@gmail.com>
2022-10-18 10:51:57 +0200

    oddPrime

---
+++ ZPRO/cvika/cv5/oddPrime.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+bool Even(int number);
+bool prime(int number);
+void printEven(bool,int);
+void printPrime(bool,int);
+bool primeWithBreak(int number);
+
+int main()
+{
+    int number{0};
+    cout << "Please enter whole number: ";
+    cin >> number;
+    printEven(Even(number),number);
+    cout << "Please enter whole possitive number to know if its prime number: ";
+    cin >> number;
+    printPrime(prime(number), number);
+    printPrime(primeWithBreak(number), number);
+    return 0;
+}
+
+bool Even(int number)
+{
+    if (number % 2 == 0){
+        return true;
+    }return false;
+}
+
+bool prime(int number)
+{
+    if (number < 2){
+        return false;
+    }
+    for (int i = 2; i*i <= number; i++){
+        if (number % i == 0){
+            return false;
+        }
+    }return true;
+}
+
+void printEven(bool isEven, int number)
+{
+    if(isEven){
+            cout << "Number " << number << " is even" << endl;
+        }else{
+            cout << "Number " << number << " is odd" << endl;
+        }
+}
+
+void printPrime(bool isPrime, int number)
+{
+    if(isPrime){
+        cout << "Number " << number << " is prime number" << endl;
+    }else{
+        cout << "Number " << number << " is not prime number" << endl;
+    }
+}
+
+bool primeWithBreak(int number)
+{
+    bool isPrime = true;
+    if (number < 2){
+        return false;
+    }
+    for (int i = 2; i*i <= number; i++){
+        if (number % i == 0){
+            isPrime = false;
+            break;
+        }
+    }return isPrime;
+}

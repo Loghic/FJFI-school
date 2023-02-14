533ec71e0619d93c529c81137695814b94a85a06
Loghic <michalek.matej.loghin@gmail.com>
2023-02-14 13:51:45 +0100

    Tweeks

--- PPY/lecture 1/HelloWorld.py
+++ PPY/lecture 1/HelloWorld.py
@@ -1,1 +1,21 @@
 print("Hello World!")
+
+x = "Hello World!"
+
+if 1 > 0:
+    print("One is more than zero")
+else:
+    print("Zero is more than one")
+print("...sound correct")
+
+print('One' if 1 > 0 else 'Zero')
+
+z = []
+
+for i in range(len(x)):
+    pos = len(x) - (i+1)
+    z.append(x[pos])
+
+string = ','.join(str(j) for j in z)
+
+print(string)

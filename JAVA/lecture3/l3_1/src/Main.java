4321c8a2edc29034b71818e5059ffbeeec12eb18
Loghic <michalek.matej.loghin@gmail.com>
2023-10-04 17:59:40 +0200

    lecture 3

--- JAVA/lecture3/l3_1/src/Main.java
+++ JAVA/lecture3/l3_1/src/Main.java
@@ -1,11 +1,57 @@
 import static java.lang.Math.*;
-
+import java.util.ArrayList;
 
 public class Main
 {
 //    static final double i = 12.3;
-    public static void main(String[] args)
-    {
+
+        static String greeting(int n)
+        {
+            switch(n)
+            {
+                case 0: return "Hi"; //case -> "Hi"
+                case 1: return "Good mo:>";
+                case 2: return "Adiooos";
+                //default:throw new Exception(" Undefined value");
+                default:
+                    try {
+                        throw new Exception(" Undefined value");
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+            }
+        }
+        static ArrayList<String> data = new ArrayList<>();
+        public static void main(String[] args)
+        {
+            data.add("Hi");
+            data.add("Good evening!");
+            for (String i: data ) //projdi vsechny data v poli, prikaz ktery umoznuje projit cely kontejner
+            {
+                System.out.println(i);
+            }
+            int j = 0;
+            cyklus: for (String i: data )
+            {
+                j++;
+                System.out.println(i);
+                if(j == 1) break cyklus; //pouzivame u vnorenych cyklu, ukoncuje
+            }
+            //System.out.println(greeting (2));
+            try // za try musi byt catch nebo finally, finally jenom jednou, catch kolikrat chceme. pak proberem jak to muzeme udelat bez finally anebo catch
+            {
+                System.out.println(greeting (6));
+                System.out.println("Nice!");
+            }
+            catch (Exception ex)
+            {
+                //Logger.getLogger(Main.src.getName () ).log(level.SEVERE, null, ex);
+                System.out.println("Error " + ex.getMessage());
+            }
+            finally
+            {
+                System.out.println("End!!");
+            }
 //        System.out.println(i);
 //        int i = 8;
 //        System.out.println(i);

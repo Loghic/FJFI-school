65a9a61716a2d05105f235d691df8b8d5b9c646f
Loghic <michalek.matej.loghin@gmail.com>
2023-10-18 09:13:26 +0200

    lecture 4 update

--- PW_C/lecture4/Paint2/Form1.cs
+++ PW_C/lecture4/Paint2/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Imaging;
+
 namespace Paint2
 {
     public partial class Form1 : Form
@@ -5,7 +7,7 @@ namespace Paint2
         public Form1()
         {
             InitializeComponent();
-            pictureBox_SizeChanged (null, null);
+            pictureBox_SizeChanged(null, null);
         }
 
         private void pictureBox_SizeChanged(object sender, EventArgs e)
@@ -21,8 +23,8 @@ namespace Paint2
             }
 
             Bitmap image = new Bitmap(w, h);
-            Brush b = new SolidBrush(Color.White);
             Graphics g = Graphics.FromImage(image);
+            Brush b = new SolidBrush(Color.White);
             g.FillRectangle(b, 0, 0, w, h);
 
             if (old != null)
@@ -31,6 +33,44 @@ namespace Paint2
             }
 
             pictureBox.Image = image;
+
+            if (false)
+            {
+                w = pictureBox.Width;
+                h = pictureBox.Height;
+                Bitmap bmp = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+                byte[] data = new byte[4 * w * h];
+                {
+                    int y = 20;
+                    for (int x = 10; x <= 80; x++)
+                    {
+                        int i = 4 * (x + y * w);
+                        data[i + 1] = 255; // Green
+                        data[i + 3] = 255; // Alpha
+                    }
+                }
+
+                for (int y = 0; y < h; y++)
+                    for (int x = 0; x < w; x++)
+                    {
+                        int i = 4 * (x + y * w);
+                        data[i] = (byte)(x % 256); // Blue
+                        data[i + 1] = (byte)(y & 0xff); // Green
+                        data[i + 2] = 0; // Red
+                        data[i + 3] = 255; // Alpha
+                    }
+
+                // http://stackoverflow.com/questions/68717508/receiving-double-image-with-bitmap-on-c-sharp
+                Rectangle dimension = new Rectangle(0, 0, bmp.Width, bmp.Height);
+                BitmapData picData = bmp.LockBits(dimension, ImageLockMode.ReadWrite, bmp.PixelFormat);
+                IntPtr pixelStartAddress = picData.Scan0;
+                System.Runtime.InteropServices.Marshal.Copy(data, 0, pixelStartAddress, data.Length);
+                bmp.UnlockBits(picData);
+
+                pictureBox.Image = bmp;
+            }
+
         }
 
         private int X0, Y0;
@@ -51,6 +91,7 @@ namespace Paint2
                 Graphics g = Graphics.FromImage(pictureBox.Image);
                 g.DrawImage(save, 0, 0);
                 Pen p = new Pen(Color.FromArgb(255, 0, 0));
+                p.Width = 3;
                 g.DrawLine(p, X0, Y0, e.X, e.Y);
                 pictureBox.Invalidate();
             }

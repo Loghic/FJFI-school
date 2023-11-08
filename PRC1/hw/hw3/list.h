a584bc2219eaf44efa602e20100f28646889ee7c
Loghic <michalek.matej.loghin@gmail.com>
2023-11-08 13:40:19 +0100

    hw3

---
+++ PRC1/hw/hw3/list.h
@@ -0,0 +1,42 @@
+#ifndef HW3_LIST_H
+#define HW3_LIST_H
+
+#include "element.h"
+
+class List{
+    Element *first;
+    Element *last;
+    unsigned int counter;
+    void initList();
+    void copyDataFrom(List &source);
+    Element* find(Data d);
+public:
+    List();
+    List(List &other);
+    ~List();
+    void clear();
+    void pushBack(Data d);
+    void pushFront(Data d);
+    unsigned int size() const{
+        return counter;
+    }
+    bool isEmpty() const{
+        return !size();
+        //return counter == 0;
+    }
+    void popFront();
+    void popBack();
+    Data front() const{
+        return first->next->getData();
+    }
+    Data back() const{
+        return last->prev->getData();
+    }
+    void print() const;
+    void append(List &other){
+        this->copyDataFrom(other);
+    }
+    bool remove(Data d);
+};
+
+#endif //HW3_LIST_H
---
+++ PRC1/hw/hw3/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "list.h"
+
+using namespace std;
+
+int main()
+{
+    List l;
+    for(int i = 0; i < 10; i++){
+        if(i % 2 == 0)
+            l.pushBack(i);
+        else
+            l.pushFront(i);
+
+    }
+    l.print();
+    return 0;
+}

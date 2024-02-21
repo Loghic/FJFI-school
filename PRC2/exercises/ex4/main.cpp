cd7f885a599e82f3a49069905188c6e3c4bbd0ae
Loghic <michalek.matej.loghin@gmail.com>
2024-02-21 09:17:16 +0100

    Filter

--- PRC2/exercises/ex4/main.cpp
+++ PRC2/exercises/ex4/main.cpp
@@ -2,10 +2,23 @@
 
 using namespace std;
 
+const int shift = 'a' - 'A';
+
+string toUpper(string &s)
+{
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z'){
+            s[i] -= shift;
+        }
+    }
+    return s;
+}
+
 int main() {
     string s;
     while(getline(cin, s)){
-        cout << s << endl;
+        cout << toUpper(s) << endl;
     }
     return 0;
 }

dbde1a2253459a79781a0b3410c984acc4f89af7
Loghic <michalek.matej.loghin@gmail.com>
2024-03-27 15:02:58 +0100

    HW unique_ptr class implementation

---
+++ PRC2/exercises/ex11 - autopointer/debug.h
@@ -0,0 +1,8 @@
+#ifndef EX11___AUTOPOINTER_DEBUG_H
+#define EX11___AUTOPOINTER_DEBUG_H
+
+#define DEBUG
+
+#undef DEBUG // we can write this when we don't need to DEBUG || comment it when we need it
+
+#endif //EX11___AUTOPOINTER_DEBUG_H
--- PRC2/exercises/ex11 - autopointer/main.cpp
+++ PRC2/exercises/ex11 - autopointer/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
 
+#include "autopointer.h"
+
+const int SIZE = 3;
+
+struct test{
+    int len = SIZE;
+    int data[SIZE]{};
+    test(){
+        for (int i = 0; i < SIZE; i++){
+            data[i] = i;
+        }
+    }
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    AutoPointer<int> p(new int(3));
+    AutoPointer<int> q(new int(5));
+    AutoPointer<int> r(p);
+    AutoPointer<test> s(new test);
+    q = p;
+
+    cout << "Size: " << s->len << endl;
+    for (int i = 0; i < SIZE; i++){
+        cout << s->data[i] << endl;
+    }
+
+    AutoPointer<test> t(s);
+
+    cout << s->len << endl;
+
+    s->data[0] = -4;
+    cout << s->data[0] << endl;
+    cout << t->data[0] << endl;
+
     return 0;
 }

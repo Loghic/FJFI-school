6b329c8e8a248355e7aa526db5bd0dc4732aef6d
Loghic <michalek.matej.loghin@gmail.com>
2024-04-03 12:37:06 +0200

    fstream, read/write from/to file

--- PRC2/lectures/lecture8/main.cpp
+++ PRC2/lectures/lecture8/main.cpp
@@ -1,6 +1,50 @@
-#include <iostream>
+#include <fstream>
+#include "iostream"
+
+using namespace std;
+
+const int N = 10;
+
+const char *NAME = "data.txt";
+
+//write to file without exceptions
+bool write_to_file(const char* name)
+{
+    ofstream output(name); // to overwrite file
+//    ofstream output(name, std::ios_base::app); // to append the file
+    if(!output){
+        return false;
+    }
+    for (int i = 1; i <= N; ++i){
+        output << i << " ";
+    }
+
+    output << endl;
+
+    output.close();
+
+    return bool(output);
+}
+
+void read_from_file(const char* name){
+    ifstream input(name);
+    //skipping controls
+    if (!input){
+        cout << "File could not be open!" << endl;
+    }
+
+    int i;
+    while(input >> i){
+        cout << i << endl;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    if (write_to_file(NAME)){
+        cout << "Done!" << endl;
+    }else{
+        cout << "ERROR!" << endl;
+    }
+    read_from_file(NAME);
     return 0;
 }

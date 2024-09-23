8e7fa9823bff83214730223e0e592392669eb154
Loghic <michalek.matej.loghin@gmail.com>
2024-09-23 17:32:41 +0200

    ADD constructors

--- UOP/lecture1/lecture1/main.cpp
+++ UOP/lecture1/lecture1/main.cpp
@@ -8,6 +8,8 @@ private:
     int x, y;
     double z;
 public:
+    Point ();
+    Point (int _x, int _y, double _z) : x(_x), y(_y), z(_z) {cout << "consts with param\n";}
     void setX(int _x) {x = _x;}
     void setY(int _y) {y = _y;}
     void setZ(double _z) {z = _z;}
@@ -23,6 +25,12 @@ public:
     void move(int newX, int newY, double newZ);
 };
 
+Point::Point()
+: x(0), y(0), z(0)
+{
+    cout << "constructor without parameters!\n";
+}
+
 void Point::move(int newX, int newY, double newZ)
 {
     x += newX;
@@ -31,15 +39,13 @@ void Point::move(int newX, int newY, double newZ)
 }
 
 int main(int argc, const char * argv[]) {
-
-    Point a;
-    a.setX(10);
-    a.setY(20);
-    a.setZ(9.2);
     
-    cout << a.getX() << " " << a.getY() << endl;
+    Point b;
+    Point a(2,3,5.5);
+    cout << a.getX() << " " << a.getY() << " " << a.getZ() << endl;
+    
     a.movePoint(30,800);
-    cout << a.getX() << " " << a.getY() << endl;
+    cout << a.getX() << " " << a.getY() << " " << a.getZ() << endl;
     
     a.move(-1000, -1000, 83.2);
     cout << a.getX() << " " << a.getY() << " " << a.getZ() << endl;
